d5351be3452b2a7dccb3b62d3a2ebc6bc96b2df6
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-13 08:14:41 +0700

    a linik

--- index.js
+++ index.js
@@ -20,16 +20,31 @@ function isiRow(tree){
 function addListDir(idlist,dirname){
     // Create a new <div> element
     var newDiv = document.createElement('div');
+
     // Add a class to the new <div> element
     newDiv.className = 'list-item';
+
     // Create a new <li> element to be nested inside the <div>
     var newListItem = document.createElement('li');
-    // Set the text content of the new <li> element
-    newListItem.textContent = dirname;
+
+    // Create a new <a> element
+    var newLink = document.createElement('a');
+
+    // Set the href attribute of the <a> element
+    newLink.href = './'+dirname; // Set the URL you want the link to point to
+
+    // Set the text content of the <a> element
+    newLink.textContent = dirname;
+
+    // Append the <a> element to the <li> element
+    newListItem.appendChild(newLink);
+
     // Append the new <li> element to the new <div>
     newDiv.appendChild(newListItem);
+
     // Get the parent <ul> element by its ID
     var ul = document.getElementById(idlist);
+
     // Append the new <div> element to the <ul>
     ul.appendChild(newDiv);
 }
