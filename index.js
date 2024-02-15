e16742eea0cd0889542c9275f4e1e0f3d437ad6a
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-16 06:27:53 +0700

    ok

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import {setInner,addChild,hide } from "https://cdn.jsdelivr.net/gh/jscroot/eleme
 import {getHash,onHashChange} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import {loading} from "./loading.js";
 
-document.title = "Repositori Surat Keputusan";
+
+const title = "Repositori Surat Keputusan";
 const repoOrg = "repoulbi";
 const urlPDFViewer = "https://repo.ulbi.ac.id/view/#";
 
@@ -11,6 +12,8 @@ const repoPathName = window.location.pathname;
 const apiURL="https://api.github.com/repos/"+repoOrg+""+repoPathName+"contents/";
 const idList = "dirlist";
 
+document.title = title;
+setInner=title;
 onHashChange(runMain);
 runMain();
 
