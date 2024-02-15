7e1bf2a1158adebe6585052422f6c396498c922c
Rolly Maulana Awangga <awangga@gmail.com>
2024-02-15 13:11:06 +0700

    index js samakan

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import {get} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {setInner,addChild,hide } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 
+let path=window.location.pathname.replace("/sk", "");
+let url = "https://api.github.com/repos/repoulbi/sk/contents/"+path;
+const urlPDFViewer = "https://repo.ulbi.ac.id/view/#";
 
-get("https://api.github.com/repos/repoulbi/sk/git/trees/main",renderHTML);
+get(url,renderHTML);
 
 function renderHTML(result){
-    let lists=result.tree;
-    console.log(lists);
-    lists.forEach(isiRow);
+    console.log(result);
+    result.forEach(isiRow);
     hide("loading");
 }
 
 function isiRow(tree){
-    if (tree.type === "tree"){
+    if (tree.type === "dir"){
         addListDir("dirlist",tree.path);
+    } else if ((tree.name.includes(".pdf")) && (tree.type==="file")){
+        let url=urlPDFViewer+btoa("#/sk/"+tree.path);
+        addListFilePdf("dirlist",tree.name,url);
     }
 
 }
@@ -32,7 +37,39 @@ function addListDir(idlist,dirname){
     var newLink = document.createElement('a');
 
     // Set the href attribute of the <a> element
-    newLink.href = './'+dirname; // Set the URL you want the link to point to
+    newLink.href = '/sk/'+dirname; // Set the URL you want the link to point to
+
+    // Set the text content of the <a> element
+    newLink.textContent = "> "+dirname;
+
+    // Append the <a> element to the <li> element
+    newListItem.appendChild(newLink);
+
+    // Append the new <li> element to the new <div>
+    newDiv.appendChild(newListItem);
+
+    // Get the parent <ul> element by its ID
+    var ul = document.getElementById(idlist);
+
+    // Append the new <div> element to the <ul>
+    ul.appendChild(newDiv);
+}
+
+function addListFilePdf(idlist,dirname,url){
+    // Create a new <div> element
+    var newDiv = document.createElement('div');
+
+    // Add a class to the new <div> element
+    newDiv.className = 'list-item';
+
+    // Create a new <li> element to be nested inside the <div>
+    var newListItem = document.createElement('li');
+
+    // Create a new <a> element
+    var newLink = document.createElement('a');
+
+    // Set the href attribute of the <a> element
+    newLink.href = url; // Set the URL you want the link to point to
 
     // Set the text content of the <a> element
     newLink.textContent = dirname;
